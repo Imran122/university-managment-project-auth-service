3a2cd013915e8c52edb24b9c975779c541cd0e28
imranhosain6554 <imran122.office@gmail.com>
2023-06-01 17:21:11 +0600

    module done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
-
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-//app.use("/api/v1", bookRoutes);
+app.use('/api/v1/users', usersRouter)
 app.get('/', (req: Request, res: Response) => {
   res.send('work successfully')
 })
