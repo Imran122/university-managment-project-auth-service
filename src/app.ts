742248901ba5f22f043d9fdc4b993326f89a6a1f
imranhosain6554 <imran122.office@gmail.com>
2023-06-03 15:48:41 +0600

    fix:

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,21 @@
 import cors from 'cors'
-import express, { Application, Request, Response } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
 import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
 
 //parse data
-
+app.get('env')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/v1/users', usersRouter)
-app.get('/', (req: Request, res: Response) => {
-  res.send('work successfully')
-})
 
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  next('server error')
+})
+//global error handeler
+app.use(globalErrorHandler)
 export default app
---
+++ src/shared/logger.ts
@@ -0,0 +1,69 @@
+import path from 'path'
+import { createLogger, format, transports } from 'winston'
+import DailyRotateFile from 'winston-daily-rotate-file'
+const { combine, timestamp, label, printf, prettyPrint } = format
+
+//custom format
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp)
+  const hour = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+  return `${date.toDateString()}-${hour}:${minutes}:${seconds} [${label}] ${level}:${message} `
+})
+
+const logger = createLogger({
+  level: 'info',
+  format: combine(
+    label({ label: 'university-system' }),
+    timestamp(),
+    myFormat,
+    prettyPrint()
+  ),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'succeses',
+        'universitysystem-%DATE%-success.log'
+      ),
+      level: 'info',
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+})
+const errorlogger = createLogger({
+  level: 'error',
+  format: combine(
+    label({ label: 'university-system' }),
+    timestamp(),
+    myFormat,
+    prettyPrint()
+  ),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'errors',
+        'universitysystem-%DATE%-error.log'
+      ),
+      level: 'error',
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+})
+export { logger, errorlogger }
